45445284b3c9428e9a58fd732609a0f8217b81d6
SGomez10 <sgomez@elpuig.xeill.net>
2025-02-20 17:53:59 +0100

    Change HelpPage for desktop and mobile, need to end userPostPage

--- src/views/HelpPage.vue
+++ src/views/HelpPage.vue
@@ -5,11 +5,15 @@
                 <ion-buttons slot="start">
                     <ion-back-button></ion-back-button>
                 </ion-buttons>
-                <ion-title>Ayuda</ion-title>
+                <ion-title>Centro de ayuda</ion-title>
             </IonToolbar>
         </IonHeader>
 
         <IonContent class="ion-padding">
+
+            <h3>¿Que es el centro de ayuda?</h3>
+            <p class="ion-text-justify">El Centro de Ayuda de nuestra app es tu guía definitiva para aprovechar al máximo todas sus funciones. Aquí encontrarás tutoriales interactivos, preguntas frecuentes y videos explicativos que te enseñarán a navegar por la app de manera sencilla. Ya seas un usuario principiante o avanzado, nuestro contenido está diseñado para resolver tus dudas y mejorar tu experiencia. ¡Explora, aprende y saca el máximo provecho de nuestra aplicación!
+            </p>
             <ion-accordion-group>
                 <ion-accordion value="first">
                     <ion-item slot="header" color="light">
@@ -31,7 +35,7 @@
                 </ion-accordion>
             </ion-accordion-group>
         </IonContent>
-
+        
     </IonPage>
 
 </template>
--- src/views/UserPostPage.vue
+++ src/views/UserPostPage.vue
@@ -1,56 +1,109 @@
 <template>
     <ion-page>
-        <ion-header>
-            <ion-toolbar>
-                <ion-buttons slot="start">
-                    <ion-back-button></ion-back-button>
-                </ion-buttons>
-            </ion-toolbar>
-        </ion-header>
-
-        <ion-content>
-            <ion-grid>
-                <ion-row>
-                    <ion-col size="8">
-                        <ion-img src="/assets/MediaImages/example.jpg" alt="Imagen"></ion-img>
-                    </ion-col>
-                    <ion-col size="4">
-                        <ion-card>
-                            <ion-card-header>
-                                <ion-card-title>Título</ion-card-title>
-                                <ion-card-subtitle>Subtítulo</ion-card-subtitle>
-                            </ion-card-header>
-                            <ion-card-content>
-                                <ion-textarea placeholder="Escribe aquí..."></ion-textarea>
-                            </ion-card-content>
-                        </ion-card>
-                    </ion-col>
-                </ion-row>
-            </ion-grid>
-        </ion-content>
+      <ion-header>
+        <ion-toolbar>
+          <ion-buttons slot="start">
+            <ion-back-button></ion-back-button>
+          </ion-buttons>
+        </ion-toolbar>
+      </ion-header>
+  
+      <ion-content>
+        <ion-grid>
+          <ion-row>
+            <ion-col size="8">
+              <ion-img v-if="!showSwiper" :src="images[currentIndex]" alt="Imagen" @click="showSwiper = true"></ion-img>
+  
+              <swiper v-if="showSwiper" :modules="modules" :pagination="{ clickable: true }" :navigation="true">
+                <swiper-slide v-for="(img, index) in images" :key="index">
+                  <img :src="img" @click="closeSwiper" style="width: 100%; cursor: pointer;" />
+                </swiper-slide>
+              </swiper>
+            </ion-col>
+  
+            <ion-col size="4">
+              <ion-item class="custom-item">
+                <ion-avatar slot="start">
+                  <img src="https://ionicframework.com/docs/demos/api/list/avatar-finn.png" alt="Avatar" />
+                </ion-avatar>
+                <div class="text-container">
+                  <h3>Finn</h3>
+                  <h5>30 de Marzo de 2024 15:36h</h5>
+                </div>
+              </ion-item>
+              <p class="description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ullamcorper elit quis iaculis tristique. 
+                Nullam mollis tempus arcu eu maximus. Vivamus auctor lorem eget dolor malesuada eleifend. Duis lacinia, neque vitae volutpat ultrices,
+                magna augue tempor odio, consectetur pretium justo erat sit amet justo.
+                Aenean mauris magna, ultricies ut consectetur a, venenatis in dolor. Cras rutrum nulla commodo nunc eleifend aliquet.
+                Donec pellentesque non enim et mollis. Aenean laoreet nunc magna, at sagittis quam rutrum eu. 
+              </p>
+            </ion-col>
+          </ion-row>
+        </ion-grid>
+      </ion-content>
     </ion-page>
-</template>
-
-<script setup lang="ts">
-    import { 
-        IonContent, 
-        IonHeader, 
-        IonPage, 
-        IonToolbar,
-        IonButtons,
-        IonBackButton,
-        IonImg,
-        IonTextarea,
-        IonCard,
-        IonCardHeader,
-        IonCardTitle,
-        IonCardSubtitle,
-        IonCardContent,
-        IonGrid,
-        IonRow,
-        IonCol
-    } from '@ionic/vue';
-</script>
-
-<style scoped>
-</style>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue';
+  import { 
+    IonContent, 
+    IonHeader, 
+    IonPage, 
+    IonToolbar,
+    IonButtons,
+    IonBackButton,
+    IonImg,
+    IonItem,
+    IonAvatar,
+    IonGrid,
+    IonRow,
+    IonCol 
+  } from '@ionic/vue';
+  import { Swiper, SwiperSlide } from 'swiper/vue';
+  import 'swiper/css';
+  import 'swiper/css/pagination';
+  import 'swiper/css/navigation';
+  import { Pagination, Navigation } from 'swiper/modules';
+  
+  const showSwiper = ref(false);
+  const currentIndex = ref(0);
+  const images = ref([
+    '../../public/MediaImages/edificio.jpg',
+    '../../public/MediaImages/puente.jpg',
+    '../../public/MediaImages/mountain.jpg'
+  ]);
+  
+  const modules = [Pagination, Navigation];
+  
+  const closeSwiper = () => {
+    showSwiper.value = false;
+  };
+  </script>
+  
+  <style scoped>
+  img {
+    cursor: pointer;
+  }
+  
+  /* Ajusta la alineación del avatar y los textos */
+  .custom-item {
+    display: flex;
+    align-items: center;
+  }
+  
+  /* Asegura que los textos estén alineados correctamente */
+  .text-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: 10px;
+  }
+  
+  /* Separa el párrafo del ion-item */
+  .description {
+    margin-top: 10px;
+  }
+  </style>
+  
