39d45cbac4541ed30d8074028691cde7105ae22e
SGomez10 <sgomez@elpuig.xeill.net>
2025-02-25 20:39:13 +0100

    Miso mensaje que el anterior pero UserTextPost avanzado

--- src/views/UserTextPost.vue
+++ src/views/UserTextPost.vue
@@ -10,22 +10,24 @@
     </ion-header>
 
     <ion-content class="ion-padding">
-      <ion-card>
-        <ion-item lines="none" >
-          <ion-avatar slot="start">
-            <img src="https://ionicframework.com/docs/demos/api/list/avatar-han.png" alt="Avatar">
-          </ion-avatar>
-          <div class="text-content">
-            <h3>Han Solo</h3>
-            <small>1 de Febrero de 2025 20:50h</small>
-          </div>
-        </ion-item>
-        <ion-card-content>
-          <div class="textoPost">
-            <p>Hola que tal todo</p>
-          </div>
-        </ion-card-content>
-      </ion-card>
+      <div class="profile-container">
+        <ion-avatar class="avatar">
+          <img src="https://ionicframework.com/docs/demos/api/list/avatar-han.png" alt="Avatar" />
+        </ion-avatar>
+        <div class="text-content">
+          <h3>Han Solo</h3>
+          <small>Descripción breve o información adicional</small>
+        </div>
+      </div>
+      <div class="text-view">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices magna leo, sit amet mollis lacus varius non. 
+        Praesent vel ipsum et lacus tincidunt tincidunt. Nam interdum urna diam, eu laoreet orci interdum vitae. 
+        Nullam congue metus at mi dignissim ornare. Curabitur non consectetur massa. Nam eget erat non est egestas malesuada vitae a nulla. 
+        Praesent eget nibh laoreet, vestibulum arcu eget, feugiat ex. 
+        Donec rutrum arcu condimentum cursus facilisis. Donec convallis, justo eget dapibus efficitur, lacus est pharetra sem, non gravida mauris ipsum non arcu. 
+        Vestibulum ornare nunc sit amet lacus dictum, at tempus dolor luctus. Sed gravida convallis lacinia. Nam fermentum sodales nibh, non laoreet leo pretium eu.
+        Vestibulum mi elit, blandit at felis non, cursus gravida libero. 
+      </div>
       
       <!-- Zona de comentarios de otros usuarios -->
 
@@ -56,10 +58,6 @@ IonToolbar,
 IonBackButton, 
 IonButtons, 
 IonAvatar, 
-IonItem,
-IonCard,
-IonCardHeader,
-IonCardContent,
 IonList,
 } from '@ionic/vue';
 import UserComment from '@/components/UserComment.vue';
@@ -72,6 +70,32 @@ import UserComment from '@/components/UserComment.vue';
 }
 .transparent-bg {
   background: transparent;
+}
+.profile-container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-bottom: 15px;
+}
+
+.avatar img {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
+
+.text-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.text-view {
+  width: 100%;
+  padding: 10px;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  color: #333;
+  text-align: justify;
 }
 
 </style>
